2a0b845e44915113ae295d571b6cbd1458433c9a
Allan Chaves <allancac@gmail.com>
2026-05-18 20:10:02 -0300

    feat(model): implementar tratamento de erros nos modelos Transaction e TransactionCustomer

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/model/Transaction.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/model/Transaction.java
@@ -2,13 +2,50 @@ package br.com.zenon.fraud.model;
 
 import java.math.BigDecimal;
 
-public record Transaction(
+public record Transaction (
         int step,
         Payment type,
         BigDecimal amount,
         TransactionCustomer origin,
         TransactionCustomer recipient,
-        int isFraud,
-        int isFlaggedFraud) {
+        boolean isFraud,
+        boolean isFlaggedFraud) {
+
+    public Transaction{}
+    public Transaction(int step, Payment type, BigDecimal amount, TransactionCustomer origin, TransactionCustomer recipient, int isFraud, int isFlaggedFraud) {
+        this(
+                validateStep(step),
+                type,
+                validateAmount(amount),
+                origin,
+                recipient,
+                mapIsFraud(isFraud),
+                mapIsFlaggedFraud(isFlaggedFraud)
+        );
+    }
+
+    private static int validateStep(int step){
+        if (step >=1) return step;
+        else throw new IllegalArgumentException("step should be positive: " + step);
+    }
+
+    private static BigDecimal validateAmount(BigDecimal amount){
+        if(amount !=null && amount.compareTo(BigDecimal.ZERO) > 0) return amount;
+        else throw new IllegalArgumentException("amount should be positive: " + amount);
+    }
+
+    private static boolean mapIsFraud(int isFraud){
+        if (isFraud == 0  ) return false;
+        else if (isFraud ==1) return true;
+        else throw new IllegalArgumentException("isFraud must be 0 or 1");
+    }
+
+    private static boolean mapIsFlaggedFraud(int isFlaggedFraud){
+        if(isFlaggedFraud == 0) return false;
+        else if(isFlaggedFraud == 1) return true;
+        else throw new IllegalArgumentException("isFlaggedFraud must be 0 or 1");
+
+    }
+
 }
 
--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/model/TransactionCustomer.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/model/TransactionCustomer.java
@@ -7,4 +7,18 @@ public record TransactionCustomer(
         BigDecimal oldBalance,
         BigDecimal newBalance){
 
-        }
+    public TransactionCustomer(String name, BigDecimal oldBalance, BigDecimal newBalance) {
+        if(name != null || !name.isBlank())
+            this.name = name;
+        else
+            throw new IllegalArgumentException("name should not be null or blank: " + name);
+        if(oldBalance !=null && oldBalance.compareTo(BigDecimal.ZERO) >= 0)
+            this.oldBalance = oldBalance;
+        else
+            throw new IllegalArgumentException("oldBalance should be positive: " + oldBalance);
+        if(newBalance !=null && newBalance.compareTo(BigDecimal.ZERO) >= 0)
+            this.newBalance = newBalance;
+        else
+            throw new IllegalArgumentException("newBalance should be positive: " + newBalance);
+    }
+}
