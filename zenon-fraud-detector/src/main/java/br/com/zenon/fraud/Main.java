1629189a3374ebf0d2a03fb71170c886ea4e97a2
Allan Chaves <allancac@gmail.com>
2026-05-18 20:21:32 -0300

    feat(main): alterar arquivo de input

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Main.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Main.java
@@ -1,20 +1,17 @@
 package br.com.zenon.fraud;
 
-import br.com.zenon.fraud.model.Transaction;
-import br.com.zenon.fraud.services.TransactionIngestor;
+import br.com.zenon.fraud.service.TransactionIngestor;
 
 import java.io.IOException;
-import java.util.List;
 
 
 public class Main {
     static void main(String[] args) throws IOException {
 
-        String transactionsFile = "data/PS_20174392719_1491204439457_log.csv";
+        String transactionsFile = "data/paysim_with_bad_data.csv";
         TransactionIngestor transactionIngestor = new TransactionIngestor(transactionsFile);
-        transactionIngestor.loadTransactions(1000);
-        List<Transaction> transactionsTest = transactionIngestor.getTransactions();
-        transactionsTest.forEach(System.out::println);
-
+        transactionIngestor.loadTransactions();
+        var transactions = transactionIngestor.getTransactions();
+        transactions.forEach(System.out::println);
     }
 }
